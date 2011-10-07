5aaf80d41a5e24f118846e49da3ca0d74ed7cdce
Hwee-Boon Yar <hboon@motionobj.com>
2011-10-07 16:23:21 +0800

    Added MOViewControllerWithActivityIndicator. Useful for prototying

---
+++ MOViewControllerWithActivityIndicator.m
@@ -0,0 +1,75 @@
+//
+//  MOViewControllerWithActivityIndicator.m
+//  Licensed under the terms of the BSD License, as specified below.
+//
+//  Created by Hwee-Boon Yar on May/6/11.
+//
+/*
+ Copyright 2011 Yar Hwee Boon. All rights reserved.
+ 
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ 
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ 
+ * Neither the name of MotionObj nor the names of its
+ contributors may be used to endorse or promote products derived from
+ this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#import "MOViewControllerWithActivityIndicator.h"
+
+@interface MOViewControllerWithActivityIndicator()
+
+@property (nonatomic,retain) UIBarButtonItem* rightBarButtonItemHiddenByActivityIndicator;
+
+@end
+
+
+@implementation MOViewControllerWithActivityIndicator
+
+@synthesize rightBarButtonItemHiddenByActivityIndicator;
+
+- (void)dealloc {
+	self.rightBarButtonItemHiddenByActivityIndicator = nil;
+
+	[super dealloc];
+}
+
+#pragma mark Progress view
+
+- (void)showActivityIndicator {
+	UIActivityIndicatorView* activityIndicatorView = [[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray] autorelease];
+	self.rightBarButtonItemHiddenByActivityIndicator = self.navigationItem.rightBarButtonItem;
+	// View need to make sure activity indicator is placed at the center of the right bar button "slot"
+	UIView* activityIndicatorHolderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 30, 20)];
+	[activityIndicatorHolderView addSubview:activityIndicatorView];
+	self.navigationItem.rightBarButtonItem = [[[UIBarButtonItem alloc] initWithCustomView:[activityIndicatorHolderView autorelease]] autorelease];
+	[activityIndicatorView startAnimating];
+}
+
+
+- (void)hideActivityIndicator {
+	self.navigationItem.rightBarButtonItem = self.rightBarButtonItemHiddenByActivityIndicator;
+}
+
+@end
