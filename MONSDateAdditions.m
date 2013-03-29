d113c9b2cfa9296d383a4053b9d8f347e21b6ee2
Hwee-Boon Yar <hboon@motionobj.com>
2013-03-29 12:28:50 +0800

    Remove unnecessary break statements after return in switch-case

--- MONSDateAdditions.m
+++ MONSDateAdditions.m
@@ -60,13 +60,11 @@
 		case 10:
 		case 12:
 			return 31;
-			break;
 		case 4:
 		case 6:
 		case 9:
 		case 11:
 			return 30;
-			break;
 		case 2:
 			return [self moIsLeapYear:aYear]? 29: 28;
 		default:
