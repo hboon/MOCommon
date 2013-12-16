338e7396aaa491b73c8856859e733d25118b28bd
Hwee-Boon Yar <hboon@motionobj.com>
2013-12-16 17:44:36 +0800

    Add UIDevice -moHardwareDescription

--- MOUIDeviceAdditions.m
+++ MOUIDeviceAdditions.m
@@ -78,4 +78,57 @@
 }
 
 
+//Courtesy of https://github.com/InderKumarRathore/UIDevice-Hardware/blob/master/UIDevice%2BHardware.m
+- (NSString*)moHardwareDescription {
+	NSString *hardware = [self moHardwareString];
+	if ([hardware isEqualToString:@"iPhone1,1"]) return @"iPhone 2G";
+	if ([hardware isEqualToString:@"iPhone1,2"]) return @"iPhone 3G";
+	if ([hardware isEqualToString:@"iPhone2,1"]) return @"iPhone 3GS";
+	if ([hardware isEqualToString:@"iPhone3,1"]) return @"iPhone 4";
+	if ([hardware isEqualToString:@"iPhone3,2"]) return @"iPhone 4";
+	if ([hardware isEqualToString:@"iPhone3,3"]) return @"iPhone 4 (CDMA)";
+	if ([hardware isEqualToString:@"iPhone4,1"]) return @"iPhone 4S";
+	if ([hardware isEqualToString:@"iPhone5,1"]) return @"iPhone 5";
+	if ([hardware isEqualToString:@"iPhone5,2"]) return @"iPhone 5 (GSM+CDMA)";
+
+	if ([hardware isEqualToString:@"iPod1,1"]) return @"iPod Touch (1 Gen)";
+	if ([hardware isEqualToString:@"iPod2,1"]) return @"iPod Touch (2 Gen)";
+	if ([hardware isEqualToString:@"iPod3,1"]) return @"iPod Touch (3 Gen)";
+	if ([hardware isEqualToString:@"iPod4,1"]) return @"iPod Touch (4 Gen)";
+	if ([hardware isEqualToString:@"iPod5,1"]) return @"iPod Touch (5 Gen)";
+
+	if ([hardware isEqualToString:@"iPad1,1"]) return @"iPad";
+	if ([hardware isEqualToString:@"iPad1,2"]) return @"iPad 3G";
+	if ([hardware isEqualToString:@"iPad2,1"]) return @"iPad 2 (WiFi)";
+	if ([hardware isEqualToString:@"iPad2,2"]) return @"iPad 2";
+	if ([hardware isEqualToString:@"iPad2,3"]) return @"iPad 2 (CDMA)";
+	if ([hardware isEqualToString:@"iPad2,4"]) return @"iPad 2";
+	if ([hardware isEqualToString:@"iPad2,5"]) return @"iPad Mini (WiFi)";
+	if ([hardware isEqualToString:@"iPad2,6"]) return @"iPad Mini";
+	if ([hardware isEqualToString:@"iPad2,7"]) return @"iPad Mini (GSM+CDMA)";
+	if ([hardware isEqualToString:@"iPad3,1"]) return @"iPad 3 (WiFi)";
+	if ([hardware isEqualToString:@"iPad3,2"]) return @"iPad 3 (GSM+CDMA)";
+	if ([hardware isEqualToString:@"iPad3,3"]) return @"iPad 3";
+	if ([hardware isEqualToString:@"iPad3,4"]) return @"iPad 4 (WiFi)";
+	if ([hardware isEqualToString:@"iPad3,5"]) return @"iPad 4";
+	if ([hardware isEqualToString:@"iPad3,6"]) return @"iPad 4 (GSM+CDMA)";
+
+	if ([hardware isEqualToString:@"i386"]) return @"Simulator";
+	if ([hardware isEqualToString:@"x86_64"]) return @"Simulator";
+
+	return @"Unknown";
+}
+
+
+//Courtesy of https://github.com/InderKumarRathore/UIDevice-Hardware/blob/master/UIDevice%2BHardware.m
+- (NSString*)moHardwareString {
+	size_t size = 100;
+	char *hw_machine = malloc(size);
+	int name[] = {CTL_HW,HW_MACHINE};
+	sysctl(name, 2, hw_machine, &size, NULL, 0);
+	NSString *hardware = [NSString stringWithUTF8String:hw_machine];
+	free(hw_machine);
+	return hardware;
+}
+
 @end
